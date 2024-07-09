b23039336be716000bf4c878c2f6221b973416af
Dinesh <dineshkarthie@gmail.com>
2024-07-09 16:13:43 +0530

    Code clean up

--- composeApp/src/commonMain/kotlin/network/ApiConstants.kt
+++ composeApp/src/commonMain/kotlin/network/ApiConstants.kt
@@ -3,6 +3,6 @@ package network
 import com.dineshworkspace.juicekadai.BuildKonfig
 
 object ApiConstants {
-    val API_KEY = BuildKonfig.FIREBASE_API_KEY
+    val API_KEY = BuildKonfig.FIREBASE_API_KEY.trim('\"')
     val DATABASE_URL = BuildKonfig.FIREBASE_DATABASE_URL.trim('\"')
 }
