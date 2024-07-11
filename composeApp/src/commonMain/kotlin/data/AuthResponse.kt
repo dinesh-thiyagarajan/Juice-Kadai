eb682ee2222186ffdf3ede23b1776806c43e11fa
Dinesh <dineshkarthie@gmail.com>
2024-07-11 15:33:38 +0530

    auth response object fix

--- composeApp/src/commonMain/kotlin/data/AuthResponse.kt
+++ composeApp/src/commonMain/kotlin/data/AuthResponse.kt
@@ -1,22 +1,34 @@
 package data
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class AuthResponse (
-    val idToken : String,
-    val email : String ,
+data class AuthResponse(
+    @SerialName("id_token")
+    val idToken: String,
+    @SerialName("email")
+    val email: String,
+    @SerialName("refresh_token")
     val refreshToken: String,
+    @SerialName("expires_in")
     val expiresIn: Int,
+    @SerialName("local_id")
     val localId: String
 )
 
 @Serializable
 data class TokenResponse(
-    val expiresIn: String,
-    val tokenType: String,
-    val refreshToken: String,
+    @SerialName("id_token")
     val idToken: String,
+    @SerialName("expires_in")
+    val expiresIn: String,
+    @SerialName("token_type")
+    val tokenType: String,
+    @SerialName("refresh_token")
+    val refreshToken: String,
+    @SerialName("user_id")
     val userId: String,
+    @SerialName("project_id")
     val projectId: String
 )
