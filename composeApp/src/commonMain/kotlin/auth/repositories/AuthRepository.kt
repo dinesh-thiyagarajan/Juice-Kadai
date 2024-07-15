acb8ea2c321a117b664ac3ac3fab67e3f51d4265
Dinesh <dineshkarthie@gmail.com>
2024-07-15 19:53:15 +0530

    Removed unwanted Auth data class

--- composeApp/src/commonMain/kotlin/auth/repositories/AuthRepository.kt
+++ composeApp/src/commonMain/kotlin/auth/repositories/AuthRepository.kt
@@ -1,6 +1,5 @@
 package auth.repositories
 
-import data.AuthResponse
 import data.Response
 import data.Status
 import dev.gitlive.firebase.Firebase
@@ -9,7 +8,7 @@ import dev.gitlive.firebase.auth.auth
 
 class AuthRepository(private val firebaseAuth: FirebaseAuth = Firebase.auth) {
 
-    suspend fun login(email: String, password: String): Response<AuthResponse> {
+    suspend fun login(email: String, password: String): Response<Status> {
         try {
             val authResult =
                 firebaseAuth.signInWithEmailAndPassword(email = email, password = password)
